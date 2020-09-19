355300b200b86453e65874047b00988c33f102a7
rrtundurwar <rrtundurwar@info-lap668.infocepts.com>
2020-09-19 09:20:12 +0530

    Added TestCase for Data driven Testing in Selenium

--- MSelPractice/src/test/java/javaBasics/DataTypes.java
+++ MSelPractice/src/test/java/javaBasics/DataTypes.java
@@ -5,7 +5,7 @@ public class DataTypes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		String abc = "";
 		
 		LearningObjects objlr = new LearningObjects();
 		
--- MSelPractice/src/test/java/selBasic/NewtoursWebsiteRegister.java
+++ MSelPractice/src/test/java/selBasic/NewtoursWebsiteRegister.java
@@ -1,16 +1,24 @@
 package selBasic;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class NewtoursWebsiteRegister {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
 
 		// Setup chrome driver and navigate to site and maximize window
@@ -18,6 +26,37 @@ public class NewtoursWebsiteRegister {
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("http://demo.guru99.com/selenium/newtours/");
 		driver.manage().window().maximize();
+		
+		// getting data from xl
+		FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "\\TestData\\RegisterCust.xlsx");
+
+		// C:\Users\rrtundurwar\workspace\MSelPractice\TestData\RegisterCust.xlsx
+
+		// creating workbook
+		XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+		// getting sheet
+		XSSFSheet worksheet = workbook.getSheet("Sheet1");
+
+		// getting nos of rows from sheet
+		int totalRows = worksheet.getLastRowNum();
+		
+		System.out.println("No of rows in sheet is " + totalRows);
+
+		for (int row = 1; row <= totalRows; row++) {
+			XSSFRow currentRow = worksheet.getRow(row);
+			String firstName = currentRow.getCell(0).getStringCellValue();
+			String lastName = currentRow.getCell(1).getStringCellValue();
+			String phone = currentRow.getCell(2).getStringCellValue();
+			String email = currentRow.getCell(3).getStringCellValue();
+			String address = currentRow.getCell(4).getStringCellValue();
+			String city = currentRow.getCell(5).getStringCellValue();
+			String state = currentRow.getCell(6).getStringCellValue();
+			String pincode = currentRow.getCell(7).getStringCellValue();
+			String country = currentRow.getCell(8).getStringCellValue();
+			String userName = currentRow.getCell(9).getStringCellValue();
+			String password = currentRow.getCell(10).getStringCellValue();
+		
 
 		// Click on Register link
 		WebElement registerLink = driver.findElement(By.linkText("REGISTER"));
@@ -26,47 +65,59 @@ public class NewtoursWebsiteRegister {
 		// Registration form details
 		// Contact Information
 		WebElement Firstname = driver.findElement(By.name("firstName"));
-		Firstname.sendKeys("Rachita");
+		Firstname.sendKeys(firstName);
 
 		WebElement Lastname = driver.findElement(By.name("lastName"));
-		Lastname.sendKeys("Tundurwar");
+		Lastname.sendKeys(lastName);
 
 		WebElement PhoneNo = driver.findElement(By.name("phone"));
-		PhoneNo.sendKeys("1234567891");
+		PhoneNo.sendKeys(phone);
 
 		WebElement EmailAddr = driver.findElement(By.id("userName"));
-		EmailAddr.sendKeys("rachitatundurwar@gmail.com");
+		EmailAddr.sendKeys(email);
 
 		// Mailing information
 		WebElement Addr = driver.findElement(By.name("address1"));
-		Addr.sendKeys("Balaji Ward");
+		Addr.sendKeys(address);
 
 		WebElement CityName = driver.findElement(By.name("city"));
-		CityName.sendKeys("Nagpur");
+		CityName.sendKeys(city);
 
 		WebElement State = driver.findElement(By.name("state"));
-		State.sendKeys("Maharashtra");
+		State.sendKeys(state);
 
 		WebElement Pincode = driver.findElement(By.name("postalCode"));
-		Pincode.sendKeys("442704");
+		Pincode.sendKeys(pincode);
 
 		WebElement CountryDropdown = driver.findElement(By.name("country"));
 		Select sel = new Select(CountryDropdown);
-		sel.selectByVisibleText("ANTARCTICA");
+		sel.selectByVisibleText(country);
 
 		// User Information
 		WebElement UserName = driver.findElement(By.id("email"));
-		UserName.sendKeys("Rachi");
+		UserName.sendKeys(userName);
 
 		WebElement Passwrd = driver.findElement(By.name("password"));
-		Passwrd.sendKeys("N@gpur123");
+		Passwrd.sendKeys(password);
 
 		WebElement ConfrmPaswrd = driver.findElement(By.name("confirmPassword"));
-		ConfrmPaswrd.sendKeys("N@gpur123");
-
+		ConfrmPaswrd.sendKeys(password);
+	
+		Thread.sleep(3000);
 		WebElement SubmitButtn = driver.findElement(By.name("submit"));
 		SubmitButtn.click();
+		
+		if(driver.getPageSource().contains(userName))
+		{
+			System.out.println("Registration completed for " + row + " record");
+		}
+		
+		else
+		{
+			System.out.println("Registration failed for " + row + " record");
+			Assert.fail();
+		}
 
 	}
-
+	}
 }
