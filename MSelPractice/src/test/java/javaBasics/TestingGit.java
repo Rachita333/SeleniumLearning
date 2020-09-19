c13e2ae41ba69b1aa95fd1f5dfc9ce130e1841c6
vishal.shinde <vishal.shinde@AIR-LAP-700021.global.internal>
2020-09-19 08:25:30 +0530

    testing Git checkin- New class Tesing Git

---
+++ MSelPractice/src/test/java/javaBasics/TestingGit.java
@@ -0,0 +1,12 @@
+package javaBasics;
+
+public class TestingGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Testing checkin in Git");
+		
+	}
+
+}
